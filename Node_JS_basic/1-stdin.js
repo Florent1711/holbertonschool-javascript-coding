1f75cde505421121daf3b0adeb1f26d28b40ffcf
Florent1711 <graphit01@hotmail.com>
2024-05-24 13:00:49 +0200

    modif de 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,7 @@
-if (require.main === module) {
-  console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?');
   process.stdin.on('data', (input) => {
     process.stdout.write(`Your name is: ${input.toString()}`);
     console.log('This important software is now closing');
     process.exit();
   });
-}
+
